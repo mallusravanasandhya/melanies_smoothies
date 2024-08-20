de6d1c2070876b5cccbab262e57f110d6a0dd0ee
mallusravanasandhya <154505620+mallusravanasandhya@users.noreply.github.com>
2024-08-20 12:20:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.functions import col,when_matched
 
 
 # Write directly to the app
@@ -8,9 +9,6 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """
 )
-from snowflake.snowpark.functions import col,when_matched
-
-import streamlit as st
 
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your Smoothie will be: ", name_on_order)
